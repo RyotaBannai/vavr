3638300d44ca8c5dc6a7b7efff5c21818d895b47
Ryota Bannai <ryotala0528@gmail.com>
2020-11-30 23:58:25 +0900

    add: vavr's stream... awesome...

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,14 +1,35 @@
 import io.vavr.collection.SortedSet;
+import io.vavr.collection.Stream;
 import io.vavr.collection.TreeSet;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 public class Sample {
     public static void main(String[] args) {
-        SortedSet<Integer> sset = TreeSet.of(2, 3, 1, 5);
-        Comparator<Integer> comparator = (a, b) -> b - a;
-        SortedSet<Integer> reversed = TreeSet.of(comparator, 2, 3, 1, 5);
+        myStream();
+    }
 
-        System.out.println(reversed);
+    private static void myStream() {
+        Stream.of(1, 2, 3).map(Objects::toString).forEach(Sample::p);
+    }
+
+    private static void myOderedSet() {
+        SortedSet<Integer> sset = TreeSet.of(2, 3, 1, 4); // ordered by default.
+        Comparator<Integer> comparator = (a, b) -> b - a;
+        SortedSet<Integer> reversed = TreeSet.of(comparator, 2, 3, 1, 4);
+
+        p(sset);
+        p(reversed); // TreeSet(4, 3, 2, 1)
+
+        SortedSet<Integer> ys = reversed.add(5);
+        p(ys); // TreeSet(5, 4, 3, 2, 1)
+
+        SortedSet<Integer> xs = sset.add(5);
+        p(xs); // TreeSet(1, 2, 3, 4, 5)
+    }
+
+    private static void p(Object input) {
+        System.out.println(input);
     }
 }
