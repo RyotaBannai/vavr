6d972ff2adb23777bcbb5cc332b868dc785a23dc
Ryota Bannai <ryotala0528@gmail.com>
2020-12-05 11:51:28 +0900

    add: wrapped up queue.

--- src/main/java/queue/Sample.java
+++ src/main/java/queue/Sample.java
@@ -8,6 +8,19 @@ import java.util.Arrays;
 
 public class Sample {
     public static void main(String[] args) {
+        queueCombination();
+    }
+
+    private static void queueCombination() {
+        Queue<Integer> queue = Queue.of(1, 2, 3);
+        Queue<Queue<Integer>> combinationed = queue.combinations(2);
+
+        p(combinationed); // Queue(Queue(1, 2), Queue(1, 3), Queue(2, 3))
+        p(combinationed.get(2)); // Queue(2, 3)
+        p(combinationed.get(2).toCharSeq()); // 23 Ooo
+    }
+
+    private static void simplePlayGround() {
         Queue<Integer> queue = Queue.of(1, 2);
         p(queue);
 
@@ -17,11 +30,10 @@ public class Sample {
         Tuple2<Integer, Queue<Integer>> dequeued = newQueue.dequeue();
         p(dequeued); // (1, Queue(2, 4, 5))
 
-        Queue<Integer> taildQueue = dequeued._2;
-        p(taildQueue); // goo. Queue(2, 4, 5)
+        Queue<Integer> tailedQueue = dequeued._2;
+        p(tailedQueue); // goo. Queue(2, 4, 5)
         p(dequeued._1); // 1
     }
-
 
     private static void p(Object... inputs) {
         Arrays.stream(inputs).forEach(System.out::println);
