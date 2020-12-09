d30c220d3c97b9a603e5a024a0e0037faf5db868
Ryota Bannai <ryotala0528@gmail.com>
2020-12-10 00:23:03 +0900

    add: handling NP with flatmap in vavr.

--- src/main/java/values/MyOption.java
+++ src/main/java/values/MyOption.java
@@ -12,24 +12,47 @@ public class MyOption {
     }
 
     private static void vavrOption() {
+        System.out.println("vavr Option");
         Option<String> maybeFoo = Option.of("foo");
+        try {
+            maybeFoo.map(foo -> (String) null)
+                    .map(definitelyNull -> definitelyNull.toUpperCase() + "bar");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        // Correct way to deal with the occurrences of null is to use flatMap
+        Option<String> maybeFooBar = maybeFoo
+                .map(foo -> (String) null)
+                .flatMap(definitelyNull -> Option.of(definitelyNull) // Option.of(null) will be translated to None
+                        .map(optionDefinitelyNull -> optionDefinitelyNull.toUpperCase() + "bar")); // Value が None だと処理されない.
+        p(1, ":", maybeFooBar); // None
+
+
+        // Or as another way to deal with null in map
+        Option<String> maybeFooBar2 = maybeFoo
+                .flatMap(foo -> Option.of((String) null))
+                .map(definitelyNone -> definitelyNone.toUpperCase() + "bar");
+        p(2, ":", maybeFooBar2);
     }
 
     private static void javaOptional() {
+        System.out.println("Java Optional");
         Optional<String> maybeFoo = Optional.of("foo");
-        p(maybeFoo); // Optional[foo]
+        p(1, ":", maybeFoo); // Optional[foo]
 
         Optional<String> maybeNone = null;
-        p(maybeNone); // null
+        p(2, ":", maybeNone); // null
 
         Optional<String> maybeFooBar = maybeFoo
                 .map(foo -> (String) null)
                 .map(definitelyNull -> definitelyNull.toUpperCase() + "bar");
-        p(maybeFooBar); // Optional.empty
-        p(maybeFooBar.isPresent()); // false
+        p(3, ":", maybeFooBar); // Optional.empty
+        p(4, ":", maybeFooBar.isPresent()); // false
     }
 
     private static void p(Object... inputs) {
-        Arrays.stream(inputs).forEach(System.out::println);
+        Arrays.stream(inputs).forEach(System.out::print);
+        System.out.println("");
     }
 }
